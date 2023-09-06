9468349af4d9d23f184bc0d1631b1f2fd8d19761
mehdi-romdhani <mehdi.romdhani@laplateforme.io>
2023-09-06 10:24:25 +0200

    Job-01

---
+++ jour-01/job-01/index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+function my_str_search(string $haystack, string $needle): int
+{
+    $count = 0;
+
+    for ($i = 0; isset($haystack[$i]); $i++) {
+        if ($haystack[$i] === $needle) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+echo (my_str_search('mehdi', 'l'));
